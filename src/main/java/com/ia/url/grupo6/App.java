20e0b44ddeb532dbfdb02907f6b85f58a0fdabf1
Axel Rodriguez <axel.rodriguez@cemaco.com>
2020-04-24 21:11:57 -0600

    Inicialización del proyecto

---
+++ src/main/java/com/ia/url/grupo6/App.java
@@ -0,0 +1,13 @@
+package com.ia.url.grupo6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Jose el papu pro :v");
+    }
+}
